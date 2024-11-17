b56be2460012f2ab7235937da84c31af55f84ccc
tvtinh625 <tinhakyu2k5@gmail.com>
2024-11-17 12:50:14 +0700

    Add project files.

---
+++ TranVanTinh_KTRAFE/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TranVanTinh_KTRAFE.Controllers
+{
+    public class HomeController : Controller
+    {
+      
+
+        public ActionResult MasterLayout()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ TranVanTinh_KTRAFE/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TranVanTinh_KTRAFE.MvcApplication" Language="C#" %>
